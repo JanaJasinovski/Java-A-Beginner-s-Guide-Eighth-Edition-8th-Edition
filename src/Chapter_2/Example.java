b9d5027ba2064ca24c1102dee21a371d117ad7ce
jana <jantschick3@gmail.com>
2022-07-21 17:36:42 +0300

    2

--- src/Chapter_2/Example.java
+++ src/Chapter_2/Example.java
@@ -2,6 +2,6 @@ package Chapter_2;
 
 public class Example {
     public static void main(String[] args) {
-        System.out.println("It's simple Java program.");
+        System.out.println("It's a simple Java program.");
     }
 }
---
+++ src/Chapter_2/Example2.java
@@ -0,0 +1,13 @@
+package Chapter_2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("num: " + num);
+
+        num *= 2;
+        System.out.print("num * 2: ");
+        System.out.print(num);
+    }
+}
