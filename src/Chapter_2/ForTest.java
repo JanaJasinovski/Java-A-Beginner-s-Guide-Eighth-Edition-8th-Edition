f5b7b01a857fbc44b138501e1ddc64a4e7580cfb
jana <jantschick3@gmail.com>
2022-07-21 20:56:21 +0300

    4

---
+++ src/Chapter_2/ForTest.java
@@ -0,0 +1,11 @@
+package Chapter_2;
+
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Value of i: " + i);
+        }
+    }
+}
