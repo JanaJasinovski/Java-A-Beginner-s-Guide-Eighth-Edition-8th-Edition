a495600d93ff7e579ed47e279309e38c3072f877
jana <jantschick3@gmail.com>
2022-07-26 19:45:11 +0300

    8

---
+++ src/Chapter_4/BasicMath.java
@@ -0,0 +1,19 @@
+package Chapter_4;
+
+public class BasicMath {
+    public static void main(String[] args) {
+        int a = 1 + 1;
+        int b = a * 3;
+        int c = b / 4;
+        int d = c - a;
+        int e = -d;
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e);
+
+        double da = 1 + 1;
+        double db = da * 3;
+        double dc = b / 4;
+        double dd = c - a;
+        double de = -d;
+        System.out.println(da + "\n" + db + "\n" + dc + "\n" + dd + "\n" + de);
+    }
+}
