0954820a2be0df5d4d50ab1262fe748eebd6f735
jana <jantschick3@gmail.com>
2022-07-26 21:56:28 +0300

    9

---
+++ src/Chapter_4/Ternary.java
@@ -0,0 +1,14 @@
+package Chapter_4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.println(i + " " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.println(i + " " + k);
+    }
+}
